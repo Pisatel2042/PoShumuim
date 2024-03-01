25f270126a1041b87c0f281b8fd83fac6dd84b8f
Жанна <Xvostik053@gmail.com>
2024-03-01 15:40:14 +0300

    Добавьте файлы проекта.

---
+++ PoShumuim/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoShumuim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите S");
+            double x  = Convert.ToDouble(Console.ReadLine());
+            double f = 2*Math.Sin(x)+3*Math.Cos(x);
+            Console.WriteLine();
+        }
+    }
+}
